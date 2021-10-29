464bd4f3d8ee0cc00a35a750734dbd791679d5a5
FusFendragon <fusfendragon@outlook.fr>
2021-10-29 15:28:02 +0200

    new init

--- groupomania/README.md
+++ groupomania/README.md
@@ -1,4 +1,4 @@
-# groupomania-social
+# groupomania
 
 ## Project setup
 ```
---
+++ users.js
@@ -0,0 +1,25 @@
+export default {
+	users: [
+		{
+			name: "François",
+			slug: "francois",
+			image: "user.png",
+			id: 1,
+			bio: "In vulputate massa vel massa cursus gravida. Aliquam sed lacus pulvinar, imperdiet risus at, lacinia dolor. Maecenas pellentesque arcu mattis iaculis efficitur. Maecenas quis sem pulvinar, sodales neque eget, efficitur odio",
+		},
+		{
+			name: "Charles",
+			slug: "charles",
+			image: "user.png",
+			id: 2,
+			bio: "In vulputate massa vel massa cursus gravida. Aliquam sed lacus pulvinar, imperdiet risus at, lacinia dolor. Maecenas pellentesque arcu mattis iaculis efficitur. Maecenas quis sem pulvinar, sodales neque eget, efficitur odio",
+		},
+		{
+			name: "Fanny",
+			slug: "fanny",
+			image: "user.png",
+			id: 3,
+			bio: "In vulputate massa vel massa cursus gravida. Aliquam sed lacus pulvinar, imperdiet risus at, lacinia dolor. Maecenas pellentesque arcu mattis iaculis efficitur. Maecenas quis sem pulvinar, sodales neque eget, efficitur odio",
+		},
+	],
+};
